9c5f95eb7d13de6ef76afce6e011db0e9d642c6a
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-06-08 12:49:23 -0500

    Add: Modal UI

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import './Modal.css';
 
 function Modal() {
-  return <div>modal</div>;
+  return (
+    <div className='modal'>
+      <div className='modal-content'>
+        <img src='/logobg.png' alt='Logo' />
+        <ul className='social-logins'>
+          <li>
+            <button>Inicia sesión con Google</button>
+          </li>
+          <li>
+            <button>Inicia sesión con Apple</button>
+          </li>
+          <li>
+            <button>Inicia sesión con Facebook</button>
+          </li>
+        </ul>
+        <form className='login-form'>
+          <div className='input-container'>
+            <div className='input-box'>
+              <input type='text' placeholder='Nombre' />
+            </div>
+            <div className='input-box'>
+              <input type='text' placeholder='Email' />
+            </div>
+            <div className='input-box'>
+              <input type='text' placeholder='Password' />
+            </div>
+          </div>
+          <button className='btn'>Regístrate</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Modal;
