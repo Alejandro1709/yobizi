a50b61ec7eb2ed59e5ef735dc071b733700ad34c
Alejandro LR <alejandro.lr.developer@hotmail.com>
2023-07-01 01:11:33 -0500

    remove: old codebase

--- src/modalStore.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from 'nanostores';
-
-export const modalStore = atom({
-  isOpen: false,
-});
-
-export const toggleModal = (state: boolean) => {
-  modalStore.set({ isOpen: state });
-};
